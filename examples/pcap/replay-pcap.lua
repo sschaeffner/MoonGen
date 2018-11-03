faa2d200bfd7fab256d0c517da64a54b5710c3b1
Sebastian Gallenmüller <gallenmu@net.in.tum.de>
2018-11-03 20:59:56 +0100

    add fixed packet rate feature to replay-pcap

--- examples/pcap/replay-pcap.lua
+++ examples/pcap/replay-pcap.lua
@@ -12,6 +12,7 @@ function configure(parser)
 	parser:argument("edev", "Device to use for egress."):args(1):convert(tonumber)
 	parser:argument("idev", "Device to use for ingress."):args(1):convert(tonumber)
 	parser:argument("file", "File to replay."):args(1)
+	parser:option("-f --fix-rate", "Fixed net data rate in Mbit per second (ignore timestamps in pcap)"):default(0):convert(tonumber):target("fixedRate")
 	parser:option("-r --rate-multiplier", "Speed up or slow down replay, 1 = use intervals from file, default = replay as fast as possible"):default(0):convert(tonumber):target("rateMultiplier")
 	parser:flag("-l --loop", "Repeat pcap file.")
 	local args = parser:parse()
@@ -23,15 +24,19 @@ function master(args)
 	local idev = device.config{port = args.idev, dropEnable = false}
 	device.waitForLinks()
 	local rateLimiter
-	if args.rateMultiplier > 0 then
+	if args.rateMultiplier > 0 and args.fixedRate > 0 then
+		print("-r and -f option cannot be set at the same time.")
+		return
+	end
+	if args.rateMultiplier > 0 or args.fixedRate > 0 then
 		rateLimiter = limiter:new(edev:getTxQueue(0), "custom")
 	end
-	mg.startTask("replay", edev:getTxQueue(0), args.file, args.loop, rateLimiter, args.rateMultiplier)
+	mg.startTask("replay", edev:getTxQueue(0), args.file, args.loop, rateLimiter, args.rateMultiplier, args.fixedRate)
 	stats.startStatsTask{txDevices = {edev}, rxDevices = {idev}}
 	mg.waitForTasks()
 end
 
-function replay(queue, file, loop, rateLimiter, multiplier)
+function replay(queue, file, loop, rateLimiter, multiplier, fixedRate)
 	local mempool = memory:createMemPool(4096)
 	local bufs = mempool:bufArray()
 	local pcapFile = pcap:newReader(file)
@@ -51,9 +56,13 @@ function replay(queue, file, loop, rateLimiter, multiplier)
 					if prev > ts then
 						ts = prev
 					end
+					sz = buf:getSize()
 					local delay = ts - prev
 					delay = tonumber(delay * 10^3) / multiplier -- nanoseconds
 					delay = delay / (8000 / linkSpeed) -- delay in bytes
+					if fixedRate > 0 then
+						delay = (sz + 4) / (fixedRate/10000)
+					end
 					buf:setDelay(delay)
 					prev = ts
 				end
