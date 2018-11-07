f74a533ba4c04453d8189d9196a0b81b147ea116
Sebastian Gallenmüller <gallenmu@net.in.tum.de>
2018-11-07 20:06:21 +0100

    add stop replay after specified number of packets

--- examples/pcap/replay-pcap.lua
+++ examples/pcap/replay-pcap.lua
@@ -14,6 +14,7 @@ function configure(parser)
 	parser:argument("file", "File to replay."):args(1)
 	parser:option("-f --fix-rate", "Fixed net data rate in Mbit per second (ignore timestamps in pcap)"):default(0):convert(tonumber):target("fixedRate")
 	parser:option("-r --rate-multiplier", "Speed up or slow down replay, 1 = use intervals from file, default = replay as fast as possible"):default(0):convert(tonumber):target("rateMultiplier")
+	parser:option("-p --packets", "Send only the number of packets specified"):default(0):convert(tonumber):target("numberOfPackets")
 	parser:flag("-l --loop", "Repeat pcap file.")
 	local args = parser:parse()
 	return args
@@ -31,12 +32,12 @@ function master(args)
 	if args.rateMultiplier > 0 or args.fixedRate > 0 then
 		rateLimiter = limiter:new(edev:getTxQueue(0), "custom")
 	end
-	mg.startTask("replay", edev:getTxQueue(0), args.file, args.loop, rateLimiter, args.rateMultiplier, args.fixedRate)
+	mg.startTask("replay", edev:getTxQueue(0), args.file, args.loop, rateLimiter, args.rateMultiplier, args.fixedRate, args.numberOfPackets)
 	stats.startStatsTask{txDevices = {edev}, rxDevices = {idev}}
 	mg.waitForTasks()
 end
 
-function replay(queue, file, loop, rateLimiter, multiplier, fixedRate)
+function replay(queue, file, loop, rateLimiter, multiplier, fixedRate, numberOfPackets)
 	local mempool = memory:createMemPool(4096)
 	local bufs = mempool:bufArray()
 	local pcapFile = pcap:newReader(file)
@@ -44,34 +45,44 @@ function replay(queue, file, loop, rateLimiter, multiplier, fixedRate)
 	local linkSpeed = queue.dev:getLinkStatus().speed
 	while mg.running() do
 		local n = pcapFile:read(bufs)
-		if n > 0 then
+		if n > 0 and numberOfPackets > 0 then
 			if rateLimiter ~= nil then
 				if prev == 0 then
 					prev = bufs.array[0].udata64
 				end
 				for i, buf in ipairs(bufs) do
-					if i > n then break end
-					-- ts is in microseconds
-					local ts = buf.udata64
-					if prev > ts then
-						ts = prev
+					if numberOfPackets > 0 then
+						if i > n then break end
+						-- ts is in microseconds
+						local ts = buf.udata64
+						if prev > ts then
+					                ts = prev
+						end
+						sz = buf:getSize()
+						local delay = ts - prev
+						delay = tonumber(delay * 10^3) / multiplier -- nanoseconds
+						delay = delay / (8000 / linkSpeed) -- delay in bytes
+						if fixedRate > 0 then
+							delay = (sz + 4) / (fixedRate/10000)
+						end
+						buf:setDelay(delay)
+						prev = ts
+						numberOfPackets = numberOfPackets - 1
+					else
+						break
 					end
-					sz = buf:getSize()
-					local delay = ts - prev
-					delay = tonumber(delay * 10^3) / multiplier -- nanoseconds
-					delay = delay / (8000 / linkSpeed) -- delay in bytes
-					if fixedRate > 0 then
-						delay = (sz + 4) / (fixedRate/10000)
-					end
-					buf:setDelay(delay)
-					prev = ts
 				end
 			end
 		else
 			if loop then
 				pcapFile:reset()
 			else
-				break
+				pcapFile:close()
+				mg.sleepMillis(1500)
+				mg.stop()
+				mg.sleepMillis(1500)
+				os.exit(0) -- ending moongen forcefully
+				return
 			end
 		end
 		if rateLimiter then
